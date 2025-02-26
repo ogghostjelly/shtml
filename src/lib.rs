8d4b7f68147b4d2fb978131090af26cf53771c40
OgGhostJelly <johnnycambodia09@gmail.com>
2025-02-26 19:21:28 +0700

    Added simple html text transformer

---
+++ src/lib.rs
@@ -0,0 +1,126 @@
+use std::fmt::{self, Write};
+
+use ogj_mal::{re, Env};
+
+fn find_open(input: &str) -> Option<usize> {
+    let mut index = 0;
+    while index < input.len() {
+        if input[index..].starts_with("@(") | input[index..].starts_with("@@(") {
+            return Some(index);
+        }
+        index += 1;
+    }
+    None
+}
+
+fn find_expr_end(input: &str) -> Option<usize> {
+    let mut index = 0;
+    let mut paren_count: u32 = 0;
+
+    while index < input.len() {
+        let ch = input.as_bytes()[index];
+        index += 1;
+
+        match ch {
+            b'(' => paren_count += 1,
+            b')' => {
+                paren_count = paren_count.checked_sub(1)?;
+                if paren_count == 0 {
+                    return Some(index)
+                }
+            },
+            _ => {},
+        }
+    }
+
+    None
+}
+
+fn split_expr(input: &str) -> (&str, &str, &str) {
+    let Some(index) = find_open(input) else {
+        return (input, "", "");
+    };
+
+    let (text, rest) = input.split_at(index);
+
+    let (lisp, rest) = match find_expr_end(rest) {
+        Some(index) => rest.split_at(index),
+        None => ("", rest),
+    };
+
+    (text, lisp, rest)
+}
+
+struct Tokenizer<'a> {
+    input: &'a str,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self { input }
+    }
+}
+
+#[derive(Default)]
+struct Token<'a> {
+    pub text: Option<&'a str>,
+    pub escaped_lisp: Option<&'a str>,
+    pub lisp: Option<&'a str>,
+}
+
+impl<'a> Iterator for Tokenizer<'a> {
+    type Item = Token<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.input.is_empty() {
+            return None;
+        }
+
+        let (text, lisp, rest) = split_expr(self.input);
+        self.input = rest;
+
+        let mut token = Token::default();
+
+        if !text.is_empty() {
+            token.text = Some(text);
+        }
+
+        if !lisp.is_empty() {
+            if lisp.starts_with("@@") {
+                token.escaped_lisp = Some(lisp);
+            } else {
+                token.lisp = Some(lisp);
+            }
+        }
+
+        Some(token)
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Format(#[from] fmt::Error),
+    #[error(transparent)]
+    Mal(#[from] ogj_mal::Error),
+}
+
+// FIXME: loops forever if an opening tag has no corresponding closing tag.
+pub fn transform(env: &Env, input: &str, mut output: impl Write) -> Result<(), Error> {
+    for token in Tokenizer::new(input) {
+        if let Some(text) = token.text {
+            write!(output, "{text}")?;
+        }
+
+        if let Some(text) = token.escaped_lisp {
+            let text = &text[1..];
+            write!(output, "{text}")?;
+        }
+
+        if let Some(lisp) = token.lisp {
+            let text = re(&env, &lisp[1..])?;
+            write!(output, "{text}")?;
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#![warn(clippy::pedantic)]
+
+use ogj_mal::{re, Env};
+use shtml::transform;
+
+const INP: &str = r#"
+<html>
+<head>
+</head>
+
+<body>
+    @(do
+        (def! r (range 0 10))
+        (str (join r "+") "=" (sum r)))
+</body>
+</html>"#;
+
 fn main() {
-    println!("Hello, world!");
+    let env = Env::default();
+
+    re(&env, &format!("(do{}\n)", include_str!("lib.mal"))).expect("`lib.mal` should be valid mal");
+
+    let mut output = String::new();
+
+    transform(&env, INP, &mut output).expect("the given data should be valid and not fail");
+
+    println!("{output}")
 }
