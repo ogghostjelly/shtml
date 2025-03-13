58def7fe0c3b25ba4b3c2dfcef36901a6d17febe
OgGhostJelly <johnnycambodia09@gmail.com>
2025-03-13 13:47:52 +0700

    Satiate clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 #![warn(clippy::pedantic)]
+#![allow(
+    clippy::similar_names
+)]
+// TODO: should add docs eventually in the future and remove this allow.
+#![allow(missing_docs, clippy::missing_errors_doc)]
 
 pub mod mal;
--- src/mal.rs
+++ src/mal.rs
@@ -27,7 +27,7 @@ pub fn transform_<'i>(env: &Env, input: &'i str, output: &mut String) -> Result<
     };
 
     if input[tag..].starts_with("@@") {
-        output.push_str(&input[..tag + 1]);
+        output.push_str(&input[..=tag]);
         return Ok(&input[tag + 2..]);
     }
 
@@ -35,14 +35,14 @@ pub fn transform_<'i>(env: &Env, input: &'i str, output: &mut String) -> Result<
     let lisp = &lisp[1..];
     output.push_str(text);
 
-    let mut _lisp = String::new();
+    let mut lisp_tmp = String::new();
 
     let (lisp, text) = if lisp.starts_with('(') {
         let end = find_closing_paren(lisp).ok_or(reader::Error::UnexpectedEof)?;
         let (lisp, text) = lisp.split_at(end + 1);
-        _lisp = lisp.to_string();
-        _lisp.insert(lisp.len() - 1, '\n');
-        (_lisp.as_str(), text)
+        lisp_tmp.push_str(lisp);
+        lisp_tmp.insert(lisp.len() - 1, '\n');
+        (lisp_tmp.as_str(), text)
     } else {
         let end = lisp
             .find(|ch: char| ch.is_whitespace() || ch == '<')
@@ -53,7 +53,7 @@ pub fn transform_<'i>(env: &Env, input: &'i str, output: &mut String) -> Result<
 
     let value = ogj_mal::re(env, lisp)?;
     if !value.is_nil() && !matches!(&value, MalVal::List(ls) if ls.is_empty()) {
-        _ = write!(output, "{value}")
+        _ = write!(output, "{value}");
     }
 
     Ok(text)
@@ -62,11 +62,15 @@ pub fn transform_<'i>(env: &Env, input: &'i str, output: &mut String) -> Result<
 pub fn transform(mut input: &str) -> Result<String, Error> {
     let env = Env::default();
     env.apply_ns(ogj_mal::js::ns());
-    re(&env, &format!("(do{}\n)", include_str!("lib.mal"))).expect("lib.mal should be valid mal");
+    setup_lib(&env);
 
     let mut output = String::with_capacity(input.len());
     while !input.is_empty() {
         input = transform_(&env, input, &mut output)?;
     }
     Ok(output)
+}
+
+fn setup_lib(env: &Env) {
+    re(env, &format!("(do{}\n)", include_str!("lib.mal"))).expect("lib.mal should be valid mal");
 }
