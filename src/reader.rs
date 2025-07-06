b5db29c08a14f08084114214bfeb27370f475c76
OgGhostJelly <johnnycambodia09@gmail.com>
2025-07-06 16:45:44 +0700

    Attempt at some kind of custom parser

--- src/reader.rs
+++ src/reader.rs
@@ -1,142 +1,204 @@
 use derive_more::Display;
-use nom::bytes::complete::escaped;
-use nom::character::complete::{char, one_of};
-use nom::combinator::value;
-use nom::error::ParseError;
-
-use nom::sequence::terminated;
-use nom::{
-    bytes::complete::{tag, take_till, take_while},
-    character::is_newline,
-    combinator::opt,
-    sequence::preceded,
-    Parser,
-};
-use nom::{InputTake, InputTakeAtPosition as _, Slice};
-use nom_span::Spanned;
 
 use crate::types::MalVal;
 
-type Span<'i> = Spanned<&'i str>;
-
 pub fn parse(input: &str) -> Result<Option<MalVal>, Error> {
-    let span = Span::new(input, true);
+    let input = Span { data: input };
 
-    println!("inp: {}", span.data());
-    println!("{:?}", string(span));
+    let res = bool(input).unwrap();
 
-    Ok(None)
+    println!("{res:?}");
+
+    todo!()
 }
 
-fn string(input: Span) -> IResult<Span, String> {
-    fn str_chars(input: Span) -> IResult<Span, Span> {
-        let (mut rest, mut chr) = input.take_split(1);
+fn bool<'i>(input: Span<'i>) -> TResult<'i, bool> {
+    Or(
+        Map(Tag("true"), |_: Span<'_>| true),
+        Map(Tag("false"), |_: Span<'_>| false),
+    )
+    .parse(input)
+}
 
-        loop {
-            if *chr == "\\" {
-                // Skip one
-                _ = rest.take_split(1);
-            } else if *chr == "\"" {
-                // End
-                break;
-            }
+fn take_int<'i>(input: Span<'i>) -> SResult<'i> {
+    let rest = match Or(Tag("+"), Tag("-")).parse(input.clone()) {
+        Ok((rest, _)) => rest,
+        Err(_) => input,
+    };
 
-            (rest, chr) = rest.take_split(1)
+    let x = digits(rest)?;
+
+    todo!()
+}
+
+fn digits<'i>(input: Span<'i>) -> SResult<'i> {
+    let Some((rest, ch)) = input.take_one() else {
+        return Err(input.err(ErrorKind::Digit));
+    };
+
+    if !ch.is_ascii_digit() {
+        return Err(input.err(ErrorKind::Digit));
+    }
+
+    Ok(rest.take_while(|ch| ch.is_ascii_digit() || ch == '_'))
+}
+
+struct Map<P, F>(P, F);
+
+impl<'i, P, F, T> Parser<'i> for Map<P, F>
+where
+    P: Parser<'i>,
+    F: Fn(P::Output) -> T,
+{
+    type Output = T;
+
+    fn parse(self, input: Span<'i>) -> TResult<'i, Self::Output> {
+        let (rest, first) = self.0.parse(input)?;
+        Ok((rest, self.1(first)))
+    }
+}
+
+struct Tag(&'static str);
+
+impl<'i> Parser<'i> for Tag {
+    type Output = Span<'i>;
+
+    fn parse(self, input: Span<'i>) -> SResult<'i> {
+        let Some((rest, first)) = input.split_offset(self.0.len()) else {
+            return Err(input.err(ErrorKind::Tag(self.0)));
+        };
+
+        if first.data == self.0 {
+            Ok((rest, first))
+        } else {
+            Err(input.err(ErrorKind::Tag(self.0)))
+        }
+    }
+}
+
+struct Or<L, R>(L, R);
+
+impl<'i, O, L, R> Parser<'i> for Or<L, R>
+where
+    L: Parser<'i, Output = O>,
+    R: Parser<'i, Output = O>,
+{
+    type Output = O;
+
+    fn parse(self, input: Span<'i>) -> TResult<'i, Self::Output> {
+        let e0 = match self.0.parse(input.clone()) {
+            Ok(res) => return Ok(res),
+            Err(e) => e,
+        };
+
+        let e1 = match self.1.parse(input) {
+            Ok(res) => return Ok(res),
+            Err(e) => e,
+        };
+
+        Err(e0.or(e1))
+    }
+}
+
+/// The input type for `Parser`, keeps track of the current line and column.
+#[derive(Debug, Clone)]
+struct Span<'i> {
+    data: &'i str,
+}
+
+impl<'i> Span<'i> {
+    fn split_offset(&self, offset: usize) -> Option<(Self, Self)> {
+        if self.data.len() < offset {
+            return None;
         }
 
-        Ok(input.take_split(rest.byte_offset()))
+        Some(self.split_offset_unchecked(offset))
     }
 
-    fn parse_str(i: Span) -> IResult<Span, Span> {
-        escaped(str_chars, '\\', one_of("\"n\\"))(i)
+    fn split_offset_unchecked(&self, offset: usize) -> (Self, Self) {
+        (
+            Self {
+                data: &self.data[offset..],
+            },
+            Self {
+                data: &self.data[..offset],
+            },
+        )
     }
 
-    let (rest, string) = preceded(char('\"'), terminated(parse_str, char('\"')))(input)?;
-
-    let string = string
-        .replace(r#"\n"#, "\n")
-        .replace(r#"\""#, "\"")
-        .replace(r#"\\"#, "\\");
-
-    Ok((rest, string))
-}
-
-fn bool(input: Span) -> IResult<Span, bool> {
-    let t = value(true, tag("true"));
-    let f = value(false, tag("false"));
-    t.or(f).parse(input)
-}
-
-fn comment(input: Span) -> IResult<Span, Span> {
-    preceded(char(';'), take_till(|c| is_newline(c as u8)))(input)
-}
-
-fn float(input: Span) -> IResult<Span, f64> {
-    let (rest, _) = take_int(input)?;
-    let (rest, _) = char('.')(rest)?;
-    let (rest, _) = opt(take_int)(rest)?;
-    let (rest, num) = input.take_split(rest.byte_offset());
-
-    match num.replace('_', "").parse() {
-        Ok(num) => Ok((rest, num)),
-        Err(e) => Err(nom::Err::Error(Error::one(input, ErrorKind::ParseFloat(e)))),
+    fn take_one(&self) -> Option<(Self, char)> {
+        let (i, ch) = self.data.char_indices().next()?;
+        let rest = Self {
+            data: &self.data[i..],
+        };
+        Some((rest, ch))
     }
-}
 
-fn int(input: Span) -> IResult<Span, i64> {
-    let (rest, num) = take_int(input)?;
+    fn take_while(&self, f: impl Fn(char) -> bool) -> (Self, Self) {
+        let chars = self.data.char_indices();
 
-    match num.replace('_', "").parse() {
-        Ok(num) => Ok((rest, num)),
-        Err(e) => Err(nom::Err::Error(Error::one(input, ErrorKind::ParseInt(e)))),
+        for (offset, chr) in chars {
+            if !f(chr) {
+                return self.split_offset_unchecked(offset);
+            }
+        }
+
+        (Self::empty(), self.clone())
+    }
+
+    #[inline]
+    fn empty() -> Self {
+        Self { data: "" }
+    }
+
+    fn err(self, kind: ErrorKind) -> Error<'i> {
+        Error::One(ErrorInner { data: self, kind })
     }
 }
 
-fn take_int(input: Span) -> IResult<Span, Span> {
-    let (rest, _) = opt(char('+').or(char('-'))).parse(input)?;
-    let (rest, _) = take_while(|c| is_digit(c))(rest)?;
-    Ok(input.take_split(rest.byte_offset()))
+type TResult<'i, T> = Result<(Span<'i>, T), Error<'i>>;
+type SResult<'i> = Result<(Span<'i>, Span<'i>), Error<'i>>;
+
+/// A type that takes a span and gives an output.
+trait Parser<'i> {
+    type Output;
+
+    fn parse(self, input: Span<'i>) -> Result<(Span<'i>, Self::Output), Error<'i>>;
 }
 
-fn is_digit(chr: char) -> bool {
-    nom::character::is_digit(chr as u8) || chr == '_'
+impl<'i, T: Fn(Span) -> TResult<O>, O> Parser<'i> for T {
+    type Output = O;
+
+    #[inline]
+    fn parse(self, input: Span<'i>) -> TResult<'i, O> {
+        self(input)
+    }
 }
 
-pub type IResult<'i, I, O> = nom::IResult<I, O, Error<'i>>;
-
-#[derive(Display, Debug)]
-#[display("{}",
-    _0.iter()
-        .map(|(inp, e)| format!("error {e:?} at: {inp:?}"))
-        .collect::<Vec<String>>()
-        .join(" and ")
-)]
-
-pub struct Error<'i>(Vec<(Span<'i>, ErrorKind)>);
+#[derive(thiserror::Error, Debug)]
+pub enum Error<'i> {
+    #[error("{0}")]
+    One(ErrorInner<'i>),
+    #[error("{0:?}")]
+    Or(Box<(Error<'i>, Error<'i>)>),
+}
 
 impl<'i> Error<'i> {
-    pub fn one(input: Span<'i>, kind: ErrorKind) -> Self {
-        Self(vec![(input, kind)])
+    pub fn or(self, o: Self) -> Self {
+        Self::Or(Box::new((self, o)))
     }
 }
-
-impl<'i> std::error::Error for Error<'i> {}
 
 #[derive(Display, Debug)]
-pub enum ErrorKind {
-    ParseInt(std::num::ParseIntError),
-    ParseFloat(std::num::ParseFloatError),
-    #[display("{_0:?}")]
-    Nom(nom::error::ErrorKind),
+#[display("error {kind:?} at: {data:?}")]
+pub struct ErrorInner<'i> {
+    data: Span<'i>,
+    kind: ErrorKind,
 }
 
-impl<'i> ParseError<Span<'i>> for Error<'i> {
-    fn from_error_kind(input: Span<'i>, kind: nom::error::ErrorKind) -> Self {
-        Self(vec![(input, ErrorKind::Nom(kind))])
-    }
-
-    fn append(input: Span<'i>, kind: nom::error::ErrorKind, mut other: Self) -> Self {
-        other.0.push((input, ErrorKind::Nom(kind)));
-        other
-    }
+#[derive(Debug)]
+pub enum ErrorKind {
+    Tag(&'static str),
+    Digit,
+    Int,
 }
